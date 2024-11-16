ae133efed4a64211706ab91ac778fea2f5e3e083
Belizario Rocha <belizariorocha@gmail.com>
2024-11-16 15:53:45 -0600

    variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+# Number of launches
+launch_count = 10
+
+# Name of the rocket
+rocket_name = 'Falcon 9'
+
+# Launch success rate
+success_rate = 0.95
+
+print(f"Number of launches = {launch_count}")
+print(f"Name of the rocket = {rocket_name}")
+print(f"Launch success rate = {success_rate}")
