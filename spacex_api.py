341f3e74a2d166cfbcd2e510f2abcffc2491dc85
Belizario Rocha <belizariorocha@gmail.com>
2024-11-16 16:09:38 -0600

    api updates

--- spacex_api.py
+++ spacex_api.py
@@ -1,9 +1,9 @@
-import requests
 import pandas as pd
 
-response = requests.get('https://api.spacexdata.com/v4/launches')
-data = response.json()
-
-df = pd.DataFrame(data)
-
+url = 'https://api.spacexdata.com/v4/launches'
+df = pd.read_json(url)
 print(df.head())
+
+print(df.columns)
+
+print(df.dtypes)
